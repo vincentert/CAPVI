8969f5cbe69d5ed96d99087a3a9d414a893a6708
Ali <Ali@DESKTOP-MUO39P2>
2018-05-15 15:13:55 +0200

    Rectification des noms de package

---
+++ Capvi/src/test/java/com/project/capvi/CapviApplicationTests.java
@@ -0,0 +1,16 @@
+package com.project.capvi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CapviApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
